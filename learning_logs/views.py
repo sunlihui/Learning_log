fbc92eae3df51847f18f3b316616021347fc98c7
sunlihui <sunlihui@tuhu.cn>
2021-06-05 17:33:52 +0800

    加了一个字"的"用来测试

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Topic
+from django.http import HttpResponseRedirect
+# 下面新版本django修改了这个配置，跟经典蟒蛇书上配置的会报错，参考https://stackoverflow.com/questions/43139081/importerror-no-module-named-django-core-urlresolvers
+from django.urls import reverse
+from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -6,7 +12,6 @@ def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
 
-from .models import Topic
 
 def topics(request):
     """显示所有的主题"""
@@ -20,3 +25,37 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method !='POST':
+        # 未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form =TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    '''在特定的主题中添加新条目'''
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic =topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',args=[topic_id]))
+
+    context = {'topic': topic,'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
